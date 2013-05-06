5dfbb00fb521653a26fd239014b2b8beb0281d43
Ruslan Kornev <oganer@gmail.com>
2013-05-06 04:27:38 +0400

    rails new cocoon_destroy

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CocoonDestroy::Application.config.secret_key_base = '9a600db31c3270be9251ce90b8c989808985747dd63f677144239096d49facac0b4ecee53fa2e1318dd044d86ec776dbc477f1d2090b7508a82b21dced0602f6'
